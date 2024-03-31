a8b1da52b57bebde01c7c9f040d1e2a0c243b546
soufianetangi2@gmail.com <soufianetangi>
2024-03-31 16:34:23 -0700

    Updated script

--- factors.c
+++ factors.c
@@ -15,13 +15,13 @@ int main(int argc, char *argv[])
 	long long flag = 1, div, rest, number, counter;
 	ssize_t nread;
 
-	if (argc != 2) {
+	if(argc != 2){
 		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
 	stream = fopen(argv[1], "r");
-	if (stream == NULL) {
+	if(stream == NULL){
 		perror("fopen");
 		exit(EXIT_FAILURE);
 	}
@@ -29,9 +29,9 @@ int main(int argc, char *argv[])
 	while ((nread = getline(&line, &len, stream)) != -1) {
 		flag = 1, div = 2;
 		number = atoll(line);
-		while (flag) {
+		while(flag){
 			rest = number % div;
-			if (!rest) {
+			if(!rest){
 				counter = number / div;
 				printf("%lld=%lld*%lld\n", number, counter, div);
 				flag = 0;
